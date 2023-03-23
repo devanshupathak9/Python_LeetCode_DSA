db0deb216496c5cbdebcdcce887f3f3ab79ea9d1
devanshu <pathakdevanshu9@gmail.com>
2023-03-24 00:06:58 +0530

    New codes

---
+++ Merge_sort.py
@@ -0,0 +1,10 @@
+def merge_sort(arr,l,r):
+	if l==r:
+		return [arr[l]]
+	mid=
+
+#arr=list(map(int,input().split()))
+arr=[2,3,6,5,1,7,9,8,0,12,10]
+n=len(arr)
+New=merge_sort(arr,0,n-1)
+print(New)
