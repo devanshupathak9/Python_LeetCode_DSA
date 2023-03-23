3fdc8b51c641e6100013e1324a7f6bfa4c8e4af3
devanshu <pathakdevanshu9@gmail.com>
2023-03-24 00:11:26 +0530

    changes

--- Merge_sort.py
+++ Merge_sort.py
@@ -1,7 +1,25 @@
 def merge_sort(arr,l,r):
 	if l==r:
 		return [arr[l]]
-	mid=
+	mid=(l+r)//2
+	lft=merge_sort(arr,l,mid)
+	rght=merge_sort(arr,mid+1,r)
+	i,j=0,0
+	newarr=[]
+	while i<len(lft) and j<len(rght):
+		if lft[i]<rght[j]:
+			newarr.append(lft[i])
+			i+=1
+		else:
+			newarr.append(rght[j])
+			j+=1
+	while i<len(lft):
+		newarr.append(lft[i])
+		i+=1
+	while j<len(rght):
+		newarr.append(rght[j])
+		j+=1
+	return newarr
 
 #arr=list(map(int,input().split()))
 arr=[2,3,6,5,1,7,9,8,0,12,10]
